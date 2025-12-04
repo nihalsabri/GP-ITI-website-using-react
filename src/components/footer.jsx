35f3f72d96ce0cc472044501d930df4623ecae2e
Nihal Sabri <nihalsabri60@gmail.com>
2025-12-04 16:34:11 +0200

    init project

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Footer () {
+    return (
+         <footer className="bg-red-500 p-8 text-center text-white">
+            <p className="text-2xl font-bold mb-2">© 2024 My Website. All rights reserved.</p>
+            
+        </footer>
+    )
+}
