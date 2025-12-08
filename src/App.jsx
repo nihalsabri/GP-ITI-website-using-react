7e2892f458675cba9886c9f62ffe2e09b12578d7
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-08 20:09:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import LandingPage from "./components/LandingPage";
 import { useSelector } from "react-redux";
 import Services from "./components/Services";
 import NotFound from "./components/NotFound";
+import AboutUs from "./components/AboutUs";
+import Contact from "./components/Contact";
+import Tradespeople from "./components/Tradespeople";
 
 const router = createBrowserRouter([
   {
@@ -24,14 +27,19 @@ const router = createBrowserRouter([
         path: "services",
         element: <Services />,
       },
-      // {
-      //   path: "about",
-      //   element: <About />,
-      // },
-      // {
-      //   path: "contact",
-      //   element: <Contact />,
-      // },
+      {
+        path: "about",
+        element: <AboutUs />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+
+      {
+        path: "tradespeople",
+        element: <Tradespeople />,
+      },
     ],
   },
   {
