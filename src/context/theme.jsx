f22ba7a455e4826d556ddd83d5f6f485afd7ce83
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-09 01:06:21 +0200

    Create theme.jsx

---
+++ src/context/theme.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [mode, setMode] = useState("light");
+
+  const toggleThemeMode = () => {
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
+
+  // const setThemeMode = (newMode) => {
+  //   setMode(newMode);
+  // };
+
+  const value = {
+    mode,
+    toggleThemeMode,
+    // setThemeMode,
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+};
