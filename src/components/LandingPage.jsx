5e25285fa1b77d9999e741f00dc8bbd22ce8216d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-08 17:40:36 +0200

    Create LandingPage.jsx

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="text-3xl font-bold w-full text-center my-2.5">
+      LandingPage
+    </div>
+  );
+};
+
+export default LandingPage;
