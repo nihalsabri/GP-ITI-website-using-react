736d47bbb4a1d4cf6abb1e3f8a9d9cca24d66d2b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-08 19:48:13 +0200

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,15 @@
+import { useSelector } from "react-redux";
+
 export default function Footer() {
+  const theme = useSelector((state) => state.theme.mode);
   return (
-    <footer className="bg-red-500 p-8 text-center text-white">
+    <footer
+      className={`${
+        theme === "dark"
+          ? "bg-gray-800 text-white"
+          : "bg-gray-200 text-gray-900"
+      }  p-4 mt-8 flex justify-center items-center`}
+    >
       <p className="text-2xl font-bold mb-2">
         © 2024 My Website. All rights reserved.
       </p>
