312d0e96e4ab6889ed9d3404c18ff1afae42fc39
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-08 17:39:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
+import { Outlet } from "react-router";
 import "./App.css";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      // {
+      //   path: "services",
+      //   element: <Services />,
+      // },
+      // {
+      //   path: "about",
+      //   element: <About />,
+      // },
+      // {
+      //   path: "contact",
+      //   element: <Contact />,
+      // },
+    ],
+  },
+]);
 
 //* REACT ONLY HAS COMPONENTS "BEST PRACTICE WISE"
 //* NEXT.js HAS PAGES
@@ -8,7 +38,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />,
     </>
   );
 }
