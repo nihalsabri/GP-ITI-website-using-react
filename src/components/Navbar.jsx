d0e75779e6bcaf05beaf548b65aeab9f3868b85b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-08 21:31:07 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,13 +27,13 @@ const Navbar = () => {
       isActive
         ? `
         text-indigo-500 
-        font-semibold
+        font-bold
         border-b-2 border-indigo-600 dark:border-indigo-400
         pb-1
       `
         : theme === "dark"
-        ? "text-gray-300 hover:text-white font-medium"
-        : "text-gray-700 hover:text-indigo-700 font-medium"
+        ? "text-gray-300 hover:text-white font-semibold"
+        : "text-gray-700 hover:text-indigo-700 font-semibold"
     }`;
 
   // mobile uses same logic but as block
@@ -42,7 +42,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`w-full sticky top-0 z-40 transition-all duration-300 ${navClass}`}
+      className={`w-full sticky top-0 z-40 py-2 transition-all duration-300 ${navClass}`}
     >
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
