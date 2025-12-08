b192482bdda68539bf522787a2faca074a7ba72f
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-08 17:40:33 +0200

    Update home.jsx
    Home is now running the outlet for the router
    
    PS. the name on my pc is Home as it should be

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,13 @@
+import { Outlet } from "react-router";
 import Footer from "./Footer";
 import Header from "./Header";
+import Navbar from "./Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Header />
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
-
+      <Navbar />
+      <Outlet />
       <Footer />
     </div>
   );
