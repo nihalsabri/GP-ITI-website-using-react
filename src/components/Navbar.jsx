59d65b3805f089840ba5e0ecc0772406b22f233e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-08 19:32:33 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+import { setThemeMode } from "../store/themeSlice";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const theme = useSelector((state) => state.theme.mode);
+  const dispatch = useDispatch();
+
+  const setTheme = () => {
+    dispatch(setThemeMode(theme === "light" ? "dark" : "light"));
+  };
+  return (
+    <div>
+      <p className="text-2xl font-medium"> Navbar </p>
+
+      <button
+        onClick={() => setTheme(theme)}
+        className={`${
+          theme === "dark"
+            ? "bg-transparent hover:bg-gray-800"
+            : "bg-gray-100 hover:bg-gray-200"
+        } px-4 py-1 rounded shadow-md`}
+      >
+        {theme === "light" ? "Light" : "Dark"}
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
