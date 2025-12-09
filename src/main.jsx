af0398e7828f867f66d20e16e4ce9f73763ae649
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-12-09 11:12:03 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { ThemeProvider } from "./context/theme.jsx";
+// import { ThemeProvider } from "./context/theme.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 
