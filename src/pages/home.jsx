dd56818227a79c88efd16d57d04d5ce15dcb1a62
Nihal Sabri <nihalsabri60@gmail.com>
2025-12-13 21:26:32 +0200

    aboutUs

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,4 @@
+import AboutUs from "../components/AboutUS";
 import Footer from "../components/footer";
 import Header from "../components/header";
 
@@ -7,7 +8,7 @@ export default function Home() {
       <Header />
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of the application.</p>
-
+<AboutUs />
       <Footer />
     </div>
   )
