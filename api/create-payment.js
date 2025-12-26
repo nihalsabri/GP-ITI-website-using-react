64be8488b1708e93f4c5b8297c43cc7262259b39
Nihal Sabri <Nihalsabri60@gmail.com>
2025-12-26 15:14:08 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 /* eslint-env node */
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY,  { apiVersion: "2024-06-20",}); // eslint-disable-line no-undef
+const stripe = new Stripe(process.env.VITE_STRIPE_SECRET_KEY,  { apiVersion: "2024-06-20",}); // eslint-disable-line no-undef
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
